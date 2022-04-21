a75e5bdc35bacc8875ec7860fc6e0943f08a4765
micomo <546513056@qq.com>
2022-04-21 20:28:29 +0800

    链表反转

--- src/test/java/com/sh/goshop/shh/NodeTest.java
+++ src/test/java/com/sh/goshop/shh/NodeTest.java
@@ -3,51 +3,98 @@ package com.sh.goshop.shh;
 import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import java.util.Queue;
 import java.util.Stack;
 
 @SpringBootTest
 public class NodeTest {
 
-    @Test
-    public void reverser(){
-        Node node = new Node(1);
+    private Node node = new Node(1);
+
+    {
         Node node1 = new Node(2);
         Node node2 = new Node(3);
         Node node3 = new Node(4);
         node2.setNext(node3);
         node1.setNext(node2);
         node.setNext(node1);
-        sout(node);
+        print(node);
+    }
 
+    /**
+     * 递归反转
+     */
+    @Test
+    public void reverserDg() {
+        node = dg(node);
+        print(node);
+    }
+
+    private Node dg(Node node) {
+        Node next = node.getNext();
+        if (next == null) {
+            return node;
+        }
+        Node head = dg(next);
+        next.setNext(node);
+        node.setNext(null);
+        return head;
+
+    }
+
+    /**
+     * 堆栈反转
+     */
+    @Test
+    public void reverser(){
         Stack<Node> nodeStack = new Stack<>();
         Node head = null;
-
         while (node != null){
             nodeStack.push(node);
             node = node.getNext();
         }
-
         if (!nodeStack.isEmpty()) {
             head = nodeStack.pop();
         }
-
         while (!nodeStack.isEmpty()){
             Node tempNode = nodeStack.pop();
             tempNode.getNext().setNext(tempNode);
             tempNode.setNext(null);
         }
-        sout(head);
+        print(head);
     }
 
-    void sout(Node node) {
+    private void print(Node node) {
         if (node == null) {
             System.out.println("---");
         } else {
             System.out.println(node.getValue());
             node = node.getNext();
-            sout(node);
+            print(node);
         }
     }
 
+}
+
+class Node {
+
+    private final int value;
+
+    private Node next;
+
+    Node(int value) {
+        this.value = value;
+        this.next = null;
+    }
+
+    int getValue() {
+        return value;
+    }
+
+    Node getNext() {
+        return next;
+    }
+
+    void setNext(Node next) {
+        this.next = next;
+    }
 }
